62966fb5f6f2b431a50f88c72676a70f3147d296
cvigueras <carlosj.vigueras@gmail.com>
2023-03-10 10:53:28 +0100

    Test: test_is_year_is_leap_year_if_is_divisible_by_400

---
+++ LeapYearTdd/LeapYearTdd.Test/LeapYearTest.cs
@@ -0,0 +1,21 @@
+﻿namespace LeapYearTdd.Test
+{
+    public class LeapYearTest
+    {
+        [Test]
+        public void test_is_year_is_leap_year_if_is_divisible_by_400()
+        {
+            var leapYear = new LeapYear();
+            var result = leapYear.IsLeapYear(400);
+            Assert.IsTrue(result);
+        }
+    }
+
+    public class LeapYear
+    {
+        public bool IsLeapYear(int year)
+        {
+            return true;
+        }
+    }
+}
